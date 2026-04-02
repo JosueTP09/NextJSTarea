907c2c214b1da536750d8ecb3d0297fc0a407057
JosueTP09 <josuetp249@gmail.com>
2026-04-02 00:16:19 +0000

    CAMBIO DE TIPOGRAFIA CON MONSERRAT (IMPORTADA EN FONTS.TS)

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,2 @@
+import {Montserrat} from 'next/font/google' //BIBILOTECA INTERNA DE NEXT.JS PARA IMPORTAR CUALQUIER TIPO DE LETRA DE GOOGLE
+export const montserrat = Montserrat({subsets: ['latin'], weight: '700'})
