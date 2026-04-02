7e2726b08496d77c663100d39bd872776e12385f
JosueTP09 <josuetp249@gmail.com>
2026-04-02 02:01:21 +0000

    RUTA DASHBOARD Y RUTAS ANIDADAS

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,5 @@
 import './ui/global.css'
-
+import {monserrat} from './ui/fonts'
 export default function RootLayout({
   children,
 }: {
@@ -9,6 +9,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={'${monserrat.className}antialiased'}>
         {children}
+        <footer className='py-10 flex justify-center items-center'>
+          Hecho con 💓 por la gente de Vercel
+        </footer>
         </body>
     </html>
   );
--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -39,7 +39,7 @@ export default function Page() {
           width={1000}
           height={760}
           className='hidden md:block'
-          />
+          />  
         </div>
       </div>
     </main>
